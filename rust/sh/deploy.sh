4ca9afb670fd1b35b1f3bb3a342df25d1c0be7e0
i18n <i18n.site@gmail.com>
2024-04-18 15:09:07 +0800

    v0.1.9

--- rust/sh/deploy.sh
+++ rust/sh/deploy.sh
@@ -6,6 +6,6 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 
 . ../../../conf/env/GITHUB_TAR.sh
-META=$(cargo metadata --format-version=1 --no-deps | jq '.packages[] | .name + ":" + .version' -r | grep "^api ")
+META=$(cargo metadata --format-version=1 --no-deps | jq '.packages[] | .name + ":" + .version' -r | grep "^api:")
 
 curl -sSf https://raw.githubusercontent.com/i18n-ops/ops/main/setup/deploy.sh | TZT_PASSWORD=$TZT_PASSWORD bash -s -- $META $@
