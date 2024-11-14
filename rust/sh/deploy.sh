e832d5e39bf3d5337bfa85663c43ac9519218a9d
i18n <i18n.site@gmail.com>
2024-11-14 21:02:41 +0800

    ♨

--- rust/sh/deploy.sh
+++ rust/sh/deploy.sh
@@ -5,27 +5,12 @@ set -e
 DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 
-DIST=../../../dist
+DIST=$(realpath ../../../dist)
 
-export PDSH_SSH_ARGS_APPEND="-q -o StrictHostKeyChecking=no"
+export PDSH_SSH_ARGS_APPEND="-q -o StrictHostKeyChecking=no -i $DIST/ssh/id_ed25519 -F /etc/ops/ansible/ssh_config"
 
 if ! command -v pdsh &>/dev/null; then
   sudo apt-get install -y pdsh
-fi
-
-if [ ! -d "~/.ssh" ]; then
-  mkdir -p ~/.ssh
-  chmod 700 ~/.ssh
-fi
-
-if [ ! -f "~/.ssh/config" ]; then
-  cp /etc/ops/ansible/ssh_config ~/.ssh/config
-  chmod 600 ~/.ssh/config
-fi
-
-if [ ! -f "~/.ssh/id_ed25519" ]; then
-  cp $DIST/ssh/id_ed25519 ~/.ssh/id_ed25519
-  chmod 600 ~/.ssh/id_ed25519
 fi
 
 . $DIST/srv_li.sh
