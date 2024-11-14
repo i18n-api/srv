45ebd4ef2e4c39189a428f5734b22887005b7629
i18n <i18n.site@gmail.com>
2024-11-14 20:56:04 +0800

    ♨

--- rust/sh/deploy.sh
+++ rust/sh/deploy.sh
@@ -6,7 +6,6 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 
 DIST=../../../dist
-. $DIST/srv_li.sh
 
 export PDSH_SSH_ARGS_APPEND="-q -o StrictHostKeyChecking=no"
 
@@ -14,14 +13,21 @@ if ! command -v pdsh &>/dev/null; then
   sudo apt-get install -y pdsh
 fi
 
-mkdir -p ~/.ssh
+if [ ! -d "~/.ssh" ]; then
+  mkdir -p ~/.ssh
+  chmod 700 ~/.ssh
+fi
+
 if [ ! -f "~/.ssh/config" ]; then
   cp /etc/ops/ansible/ssh_config ~/.ssh/config
   chmod 600 ~/.ssh/config
 fi
+
 if [ ! -f "~/.ssh/id_ed25519" ]; then
   cp $DIST/ssh/id_ed25519 ~/.ssh/id_ed25519
   chmod 600 ~/.ssh/id_ed25519
 fi
+
+. $DIST/srv_li.sh
 
 sudo pdsh -w "$SRV_LI" -l root -R ssh /opt/ops/srv/setup.sh
