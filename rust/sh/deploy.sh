92d5dc0a0fb70b68b7f39671aecb6622117c95de
i18n <i18n.site@gmail.com>
2024-11-14 20:50:54 +0800

    ♨

--- rust/sh/deploy.sh
+++ rust/sh/deploy.sh
@@ -16,10 +16,12 @@ fi
 
 mkdir -p ~/.ssh
 if [ ! -f "~/.ssh/config" ]; then
-  ln -s /etc/ops/ansible/ssh_config ~/.ssh/config
+  cp /etc/ops/ansible/ssh_config ~/.ssh/config
+  chmod 600 ~/.ssh/config
 fi
 if [ ! -f "~/.ssh/id_ed25519" ]; then
   cp $DIST/ssh/id_ed25519 ~/.ssh/id_ed25519
+  chmod 600 ~/.ssh/id_ed25519
 fi
 
 sudo pdsh -w "$SRV_LI" -l root -R ssh /opt/ops/srv/setup.sh
